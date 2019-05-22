4dcad4d4d4e517622aa211834224c15d3638fa4c
Geoffrey Davis <contact@geoffreydavis.com>
2019-05-21 21:22:18 -0500

    Initial commit.

---
+++ src/scratch/main.c
@@ -0,0 +1,23 @@
+/*!
+ * \file main.c
+ *
+ * \par Copyright
+ * Copyright (C) 1999-2019 scratchmud.org
+ * All rights reserved.
+ *
+ * \author Geoffrey Davis <gdavis@scratchmud.org>
+ * \addtogroup main
+ */
+#include <main.h>
+#include <scratch/scratch.h>
+
+/*!
+ * Program entry point.
+ * \addtogroup main
+ * \param argc the number of command line arguments
+ * \param argv an array containing the command line arguments
+ * \return zero for normal program termination, non-zero otherwise
+ */
+int main(int argc, const char *argv[]) {
+  return (EXIT_SUCCESS);
+}
---
+++ src/scratch/main.h
@@ -0,0 +1,25 @@
+/*!
+ * \file main.h
+ *
+ * \par Copyright
+ * Copyright (C) 1999-2019 scratchmud.org
+ * All rights reserved.
+ *
+ * \author Geoffrey Davis <gdavis@scratchmud.org>
+ * \addtogroup main
+ */
+#ifndef _SCRATCH_MAIN_H_
+#define _SCRATCH_MAIN_H_
+
+#include <scratch/scratch.h>
+
+/*!
+ * Program entry point.
+ * \addtogroup main
+ * \param argc the number of command line arguments
+ * \param argv an array containing the command line arguments
+ * \return zero for normal program termination, non-zero otherwise
+ */
+int main(int argc, const char *argv[]);
+
+#endif /* _SCRATCH_MAIN_H_ */
